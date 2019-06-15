c2e3ba6f96f8c9f3b5a0f80258d499026aa4074c
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2019-06-14 22:20:08 -0500

    added Cloudstar Project files

---
+++ Cloudstar/Cloudstar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloudstar
+//
+//  Created by DeAngelo Deshawn Wilson on 6/14/19.
+//  Copyright © 2019 DeAngelo Deshawn Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
