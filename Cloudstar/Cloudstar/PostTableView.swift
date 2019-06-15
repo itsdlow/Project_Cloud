e49ad9a50eadfe58d11728fe542e60f394ec8e11
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2019-06-15 11:32:05 -0500

    added table view cocoa files

---
+++ Cloudstar/Cloudstar/PostTableView.swift
@@ -0,0 +1,21 @@
+//
+//  PostTableView.swift
+//  Cloudstar
+//
+//  Created by DeAngelo Deshawn Wilson on 6/15/19.
+//  Copyright © 2019 DeAngelo Deshawn Wilson. All rights reserved.
+//
+
+import UIKit
+
+class PostTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Cloudstar/Cloudstar/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Cloudstar
+//
+//  Created by DeAngelo Deshawn Wilson on 6/15/19.
+//  Copyright © 2019 DeAngelo Deshawn Wilson. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
